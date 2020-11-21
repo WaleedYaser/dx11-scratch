3173ca308a404dc764014866a9117fba777ecd79
WaleedYaser <waleedyaser95@gmail.com>
2020-11-21 12:59:56 +0200

    render a triangle

--- example_triangle.cpp
+++ example_triangle.cpp
@@ -1,10 +1,12 @@
 #pragma comment(lib, "user32.lib")
 #pragma comment(lib, "D3D11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
 
 #define WIN32_LEAN_AND_MEAN
 #define UNICODE
 #include <Windows.h>
 #include <d3d11.h>
+#include <d3dcompiler.h>
 
 #if defined(min)
 #undef min
@@ -115,6 +117,158 @@ WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdSho
         back_buffer->Release();
     }
 
+    // create vertices buffer
+    ID3D11Buffer *vertex_buffer = nullptr;
+    {
+        float vertices[] = {
+             0.0f,  0.5f,
+             0.5f, -0.5f,
+            -0.5f, -0.5f
+        };
+
+        D3D11_BUFFER_DESC buffer_desc = {};
+        buffer_desc.ByteWidth = sizeof(vertices);
+        buffer_desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+        buffer_desc.StructureByteStride = 2 * sizeof(float);
+
+        D3D11_SUBRESOURCE_DATA subresource_data = {};
+        subresource_data.pSysMem = vertices;
+
+        HRESULT result = device->CreateBuffer(&buffer_desc, &subresource_data, &vertex_buffer);
+        if (FAILED(result))
+        {
+            OutputDebugString(L"Failed to create vertex buffer");
+            return GetLastError();
+        }
+    }
+
+    // create vertex and pixel shaders
+    ID3DBlob *vertex_shader_blob = nullptr;
+    ID3D11VertexShader *vertex_shader = nullptr;
+    ID3D11PixelShader *pixel_shader = nullptr;
+    {
+        const char shader_src[] = R"(
+            float4 vs_main(float2 position : Position) : SV_Position
+            {
+                return float4(position, 0, 1);
+            }
+
+            float4 ps_main() : SV_Target
+            {
+                return float4(1, 1, 1, 1);
+            }
+        )";
+
+        // compile vertex shader
+        {
+            ID3DBlob *error_blob = nullptr;
+            HRESULT result = D3DCompile(
+                shader_src,
+                sizeof(shader_src),
+                nullptr,
+                nullptr,
+                nullptr,
+                "vs_main",
+                "vs_5_0",
+                0,
+                0,
+                &vertex_shader_blob,
+                &error_blob);
+            if (FAILED(result))
+            {
+                OutputDebugString(L"Failed to compile vertex shader");
+                OutputDebugStringA((char *)error_blob->GetBufferPointer());
+                return GetLastError();
+            }
+        }
+
+        // compile pixel shader
+        ID3DBlob *pixel_shader_blob = nullptr;
+        {
+            ID3DBlob *error_blob = nullptr;
+            HRESULT result = D3DCompile(
+                shader_src,
+                sizeof(shader_src),
+                nullptr,
+                nullptr,
+                nullptr,
+                "ps_main",
+                "ps_5_0",
+                0,
+                0,
+                &pixel_shader_blob,
+                &error_blob);
+            if (FAILED(result))
+            {
+                OutputDebugString(L"Failed to compile pixel shader");
+                OutputDebugStringA((char *)error_blob->GetBufferPointer());
+                return GetLastError();
+            }
+        }
+
+        // create vertex shader
+        {
+            HRESULT result = device->CreateVertexShader(
+                vertex_shader_blob->GetBufferPointer(),
+                vertex_shader_blob->GetBufferSize(),
+                nullptr,
+                &vertex_shader);
+            if (FAILED(result))
+            {
+                OutputDebugString(L"Failed to create vertex shader");
+                return GetLastError();
+            }
+        }
+
+        // create pixel shader
+        {
+            HRESULT result = device->CreatePixelShader(
+                pixel_shader_blob->GetBufferPointer(),
+                pixel_shader_blob->GetBufferSize(),
+                nullptr,
+                &pixel_shader);
+            if (FAILED(result))
+            {
+                OutputDebugString(L"Failed to create pixel shader");
+                return GetLastError();
+            }
+            pixel_shader_blob->Release();
+        }
+    }
+
+    // create input layout
+    ID3D11InputLayout *input_layout = nullptr;
+    {
+        D3D11_INPUT_ELEMENT_DESC input_element_desc[] = {
+            {"Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0}
+        };
+
+        HRESULT result = device->CreateInputLayout(
+            input_element_desc,
+            ARRAYSIZE(input_element_desc),
+            vertex_shader_blob->GetBufferPointer(),
+            vertex_shader_blob->GetBufferSize(), &input_layout);
+        if (FAILED(result))
+        {
+            OutputDebugString(L"Failed to create input layout");
+            return GetLastError();
+        }
+        vertex_shader_blob->Release();
+    }
+
+    // create viewport
+    D3D11_VIEWPORT viewport = {};
+    {
+        // get window width and height
+        RECT rect;
+        GetClientRect(hwnd, &rect);
+        int window_width = rect.right - rect.left;
+        int window_height = rect.bottom - rect.top;
+
+        viewport.Width = (float)window_width;
+        viewport.Height = (float)window_height;
+    }
+
     // msg loop
     bool running = true;
     while (running)
@@ -132,12 +286,37 @@ WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdSho
         }
 
         // clear frame using red color
-        float clear_color[4] = {1.0f, 0.0f, 0.0f, 1.0f};
+        float clear_color[4] = {0.0f, 0.0f, 0.0f, 1.0f};
         context->ClearRenderTargetView(render_target_view, clear_color);
+
+        // set vertex buffer in input assempler stage
+        UINT stride = 2 * sizeof(float);
+        UINT offset = 0;
+        context->IASetVertexBuffers(0, 1, &vertex_buffer, &stride, &offset);
+        context->IASetInputLayout(input_layout);
+        context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+        // set vertex and pixel shaders
+        context->VSSetShader(vertex_shader, nullptr, 0);
+        context->PSSetShader(pixel_shader, nullptr, 0);
+
+        // set viewport
+        context->RSSetViewports(1, &viewport);
+
+        // set render target and viewport
+        context->OMSetRenderTargets(1, &render_target_view, nullptr);
+
+        // draw
+        context->Draw(3, 0);
+
         swapchain->Present(1, 0);
     }
 
     // release resources
+    input_layout->Release();
+    pixel_shader->Release();
+    vertex_shader->Release();
+    vertex_buffer->Release();
     render_target_view->Release();
     context->Release();
     device->Release();
