fe6094952bd07e685ef215b807b3503bc89d2eb6
WaleedYaser <waleedyaser95@gmail.com>
2020-12-02 21:25:52 +0200

    add stb and imgui submodules

--- example_texture.cpp
+++ example_texture.cpp
@@ -17,7 +17,7 @@
 #endif
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "external/stb_image.h"
+#include "stb/stb_image.h"
 
 LRESULT CALLBACK
 window_proc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
